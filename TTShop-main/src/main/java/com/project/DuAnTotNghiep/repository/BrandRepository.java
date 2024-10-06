6e30c8f53f16253596d4698084513fc3e962c105
Hung <hungnv2003bg@gmail.com>
2024-10-06 16:55:57 +0700

    config

---
+++ TTShop-main/src/main/java/com/project/DuAnTotNghiep/repository/BrandRepository.java
@@ -0,0 +1,13 @@
+package com.project.DuAnTotNghiep.repository;
+
+import com.project.DuAnTotNghiep.entity.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BrandRepository extends JpaRepository<Brand, Long> {
+    boolean existsByCode(String code);
+    List<Brand> findAllByDeleteFlagFalse();
+    boolean existsByName(String name);
+    boolean existsByNameAndIdNot(String name, Long id);
+}
