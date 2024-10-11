5ff0ba25064593810448d4c9dad1352b13f791ac
DESKTOP-PJ70VNQ\hduc0 <duchkph28102@fpt.edu.vn>
2024-10-11 22:09:09 +0700

    update quản lý sản phẩm

---
+++ SD123/src/main/java/com/project/DuAnTotNghiep/service/BrandService.java
@@ -0,0 +1,29 @@
+package com.project.DuAnTotNghiep.service;
+
+import com.project.DuAnTotNghiep.dto.Brand.BrandDto;
+import com.project.DuAnTotNghiep.entity.Brand;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BrandService {
+
+    Page<Brand> getAllBrand(Pageable pageable);
+    boolean existsByName(String name);
+    boolean existsByNameAndIdNot(String name, Long id);
+    Brand createBrand(Brand brand);
+    Brand updateBrand(Long id, Brand brand);
+    Brand save(Brand brand);
+
+    void delete(Long id);
+
+    Optional<Brand> findById(Long id);
+
+    List<Brand> getAll();
+
+    BrandDto createBrandApi(BrandDto brandDto);
+
+    boolean existsById(Long id);
+}
